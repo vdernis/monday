9642a2e0fffcb7f278681db092f6fd2458908831
Vasily Dernis <vdernis@gmail.com>
2024-01-22 13:50:02 -0500

    testing

--- monday/query_joins.py
+++ monday/query_joins.py
@@ -90,7 +90,9 @@ def get_item_query(board_id, column_id, value):
                         value
                         ...on MirrorValue {
                             display_value
-                            mirrored_id: id
+                            MirroredItems: mirrored_items {
+                                id
+                            }
                         }
                     }
                 }
